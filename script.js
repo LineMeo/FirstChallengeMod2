9888ac7f6d3e023b52a74cd25439a323c875160f
LineMeo <aline.marqes@gmail.com>
2022-06-09 21:04:18 -0300

    first challenge módulo 2

---
+++ script.js
@@ -0,0 +1,28 @@
+/* Primeiro Desafio do Módulo 2 - VnW
+01 - Estrutura de Dados */
+
+// Crie uma variável que receba um nome;
+
+// Crie uma variável que receba um número;
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+
+Console.log('Desafio Mod 2(VnW) - 01 - Estrutura de Dados')
+let name = 'Aline MEO'
+console.log(name)
+let number = 10
+let year = 2022 - number
+console.log(number + year)
+console.log("Estou mexendo no console.log")
+let quartaFeira = true
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
